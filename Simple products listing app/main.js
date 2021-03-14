dc048a6fda86cfe0add6a7f3eddcbd59a388ffe7
ankushduacodes <ankushduacodes@gmail.com>
2021-03-14 08:53:49 +0530

    code formatted

--- Simple products listing app/main.js
+++ Simple products listing app/main.js
@@ -8,7 +8,7 @@ const port = 3000;
 
 app.use(bodyParser.json())
 app.use(express.urlencoded({
-    extended: true
+  extended: true
 }))
 app.use(express.static(__dirname + "/static"))
 
@@ -17,78 +17,78 @@ const productCardTemplate = fs.readFileSync(`${__dirname}/templates/card-templat
 const productDescriptionTemplate = fs.readFileSync(`${__dirname}/templates/product-description-template.html`, 'utf-8')
 
 function renderProduct(product, productCardTemplate) {
-    let output = productCardTemplate.replace(/{%ID%}/g, product.id)
-    output = output.replace(/{%IMAGE%}/g, product.image)
-    output = output.replace(/{%NAME%}/g, product.name)
-    output = output.replace(/{%ORGANIC%}/g, product.organic)
-    output = output.replace(/{%QUANTITY%}/g, product.quantity)
-    output = output.replace(/{%PRICE%}/g, product.price)
-    output = output.replace(/{%DESCRIPTION%}/g, product.description)
-    return output;
+  let output = productCardTemplate.replace(/{%ID%}/g, product.id)
+  output = output.replace(/{%IMAGE%}/g, product.image)
+  output = output.replace(/{%NAME%}/g, product.name)
+  output = output.replace(/{%ORGANIC%}/g, product.organic)
+  output = output.replace(/{%QUANTITY%}/g, product.quantity)
+  output = output.replace(/{%PRICE%}/g, product.price)
+  output = output.replace(/{%DESCRIPTION%}/g, product.description)
+  return output;
 }
 
 const createProductId = () => {
-    const productIds = new Set(
-        JSON.parse(
-            fs.readFileSync(
-                `${__dirname}/products.json`, 'utf-8'
-            )).map(product => product.id)
-    );
-    let randomId = Math.floor(Math.random() * 10000000)
-    while(productIds.has(randomId)) {
-        // add retries to limit how many times loop fails before we tell user that product could not be added
-        randomId = Math.floor(Math.random() * 10000000)
-    }
-    return randomId;
+  const productIds = new Set(
+    JSON.parse(
+      fs.readFileSync(
+        `${__dirname}/products.json`, 'utf-8'
+      )).map(product => product.id)
+  );
+  let randomId = Math.floor(Math.random() * 10000000)
+  while (productIds.has(randomId)) {
+    // add retries to limit how many times loop fails before we tell user that product could not be added
+    randomId = Math.floor(Math.random() * 10000000)
+  }
+  return randomId;
 }
 
 app.get('/', (req, res) => {
-    const products = JSON.parse(fs.readFileSync(`${__dirname}/products.json`, 'utf-8'));
-    const productsTemplate = products.map(
-        (product) => renderProduct(product, productCardTemplate)
-    ).join('')
-    res.send(overviewTemplate.replace(/{%PRODUCT_CARDS%}/g, productsTemplate))
+  const products = JSON.parse(fs.readFileSync(`${__dirname}/products.json`, 'utf-8'));
+  const productsTemplate = products.map(
+    (product) => renderProduct(product, productCardTemplate)
+  ).join('')
+  res.send(overviewTemplate.replace(/{%PRODUCT_CARDS%}/g, productsTemplate))
 });
 
 
 app.get("/product", (req, res) => {
-    const products = JSON.parse(fs.readFileSync(`${__dirname}/products.json`, 'utf-8'));
-    const id = Number(req.query.id);
-    const product = products.find(product => product.id === id);
-    if (!product) {
-        res.status(404);
-        res.end("<h1>Product not found</h1>");
-    }
-    const productdescTemplate = renderProduct(product, productDescriptionTemplate)
-    res.end(productdescTemplate, "utf-8");
+  const products = JSON.parse(fs.readFileSync(`${__dirname}/products.json`, 'utf-8'));
+  const id = Number(req.query.id);
+  const product = products.find(product => product.id === id);
+  if (!product) {
+    res.status(404);
+    res.end("<h1>Product not found</h1>");
+  }
+  const productdescTemplate = renderProduct(product, productDescriptionTemplate)
+  res.end(productdescTemplate, "utf-8");
 })
 
 // TODO: add server side validations
 app.get("/add", (req, res) => {
-    res.sendFile(`${__dirname}/templates/form.html`);
+  res.sendFile(`${__dirname}/templates/form.html`);
 })
 
 app.post("/add", (req, res) => {
-    const products = JSON.parse(fs.readFileSync(`${__dirname}/products.json`, 'utf-8'));
-    const newProduct = req.body;
-    newProduct.id = createProductId();
-    newProduct.organic = !newProduct.organic ? false : true;
-    products.push(newProduct);
-    fs.writeFileSync(`${__dirname}/products.json`, JSON.stringify(products, null, 2), 'utf-8')
-    res.redirect('/');
+  const products = JSON.parse(fs.readFileSync(`${__dirname}/products.json`, 'utf-8'));
+  const newProduct = req.body;
+  newProduct.id = createProductId();
+  newProduct.organic = !newProduct.organic ? false : true;
+  products.push(newProduct);
+  fs.writeFileSync(`${__dirname}/products.json`, JSON.stringify(products, null, 2), 'utf-8')
+  res.redirect('/');
 })
 
 app.delete("/delete", (req, res) => {
-    let products = JSON.parse(fs.readFileSync(`${__dirname}/products.json`, 'utf-8'));
-    const id = Number(req.query.id);
-    const product = products.find(product => product.id === id);
-    if (!product) {
-        res.status(404);
-        res.end("<h1>Product not found</h1>");
-    }
-    products = products.filter(item => item !== product);
-    fs.writeFileSync(`${__dirname}/products.json`, JSON.stringify(products, null, 2), 'utf-8')
-    res.send(JSON.stringify({'message': 'success'}));
+  let products = JSON.parse(fs.readFileSync(`${__dirname}/products.json`, 'utf-8'));
+  const id = Number(req.query.id);
+  const product = products.find(product => product.id === id);
+  if (!product) {
+    res.status(404);
+    res.end("<h1>Product not found</h1>");
+  }
+  products = products.filter(item => item !== product);
+  fs.writeFileSync(`${__dirname}/products.json`, JSON.stringify(products, null, 2), 'utf-8')
+  res.send(JSON.stringify({'message': 'success'}));
 })
 
 app.listen(port, err => err ? console.log(err) : console.log(`app listening on http://localhost:${port}`))
--- Simple products listing app/static/js/delete.js
+++ Simple products listing app/static/js/delete.js
@@ -1,31 +1,31 @@
 async function deleteProduct(id) {
-    const xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = async function () {
-        if (xmlhttp.readyState !== XMLHttpRequest.DONE || xmlhttp.status !== 200) {
-            console.log('There was a problem with the request.');
-        } else {
-            await getHomePage();
-        }
+  const xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = async function () {
+    if (xmlhttp.readyState !== XMLHttpRequest.DONE || xmlhttp.status !== 200) {
+      console.log('There was a problem with the request.');
+    } else {
+      await getHomePage();
     }
-    const url = `http://localhost:3000/delete?id=${id}`;
-    await xmlhttp.open('DELETE', url, true);
-    await xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    await xmlhttp.send();
+  }
+  const url = `http://localhost:3000/delete?id=${id}`;
+  await xmlhttp.open('DELETE', url, true);
+  await xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  await xmlhttp.send();
 }
 
 
 async function getHomePage() {
-    const xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = async function () {
-        if (xmlhttp.readyState !== XMLHttpRequest.DONE || xmlhttp.status !== 200) {
-            console.log('There was a problem with the request.');
-        } else {
-            document.open();
-            document.write(xmlhttp.responseText);
-        }
+  const xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = async function () {
+    if (xmlhttp.readyState !== XMLHttpRequest.DONE || xmlhttp.status !== 200) {
+      console.log('There was a problem with the request.');
+    } else {
+      document.open();
+      document.write(xmlhttp.responseText);
     }
-    const url = `http://localhost:3000/`;
-    await xmlhttp.open('GET', url, true);
-    await xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    await xmlhttp.send();
+  }
+  const url = `http://localhost:3000/`;
+  await xmlhttp.open('GET', url, true);
+  await xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  await xmlhttp.send();
 }
