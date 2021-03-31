163be56c1ab1e51797386c8d161a2c6592d55bb2
ankushduacodes <ankushduacodes@gmail.com>
2021-03-31 22:08:48 +0530

    added favicon

---
+++ Simple products listing app/models/productModel.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose.Schema;
+
+const ProductSchema = new Schema({
+  name: {
+    type: String,
+    match: /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆŠŽ∂ð ,.'-]+$/,
+    trim: true,
+    required: true,
+  },
+  image: {
+    type: String,
+    match: /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/,
+    trim: true,
+    required: true,
+  },
+  organic: {
+    type: Boolean,
+    required: true,
+  },
+  quantity: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  id: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = {
+  Product: mongoose.model('Product', ProductSchema),
+};
